cb72febeb522ec59388205fcfbc5370fc0f4feed
d-cab <diegocabral209@gmail.com>
2026-06-02 14:53:44 -0700

    added CA5 but did not do

--- CA5/sample-py.py
+++ CA5/sample-py.py
@@ -1,3 +1,7 @@
+# n cities, k antennas, each city i has P(i) powerlevel
+# find minimum range 0
+
+
 def compute_min_radius(P, A, k):
     # Your code here. Return the minimum possible d times 60 
     pass
@@ -9,3 +13,9 @@ for _ in range(tc):
     P = list(map(int, input().split()))
     A = list(map(int, input().split()))
     print(compute_min_radius(P, A, k))
+
+print(tc)
+print(n)
+print(k)
+print(P)
+print(A)
